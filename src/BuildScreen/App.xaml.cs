cadb8886fe6490972469106e9d846d5181df5add
Alex Østergaard <aos@miracle.dk>
2018-11-22 13:45:00 +0100

    Initial commit

---
+++ src/BuildScreen/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using BuildScreen.Core.Shell;
+
+namespace BuildScreen
+{
+    public partial class App : Application, ISingleInstanceApp
+    {
+        private const string Unique = "F46D2B3FFBCB4c5aA1C746B37A7AB6E8";
+
+        [STAThread]
+        public static void Main()
+        {
+            try
+            {
+                if (SingleInstance<App>.InitializeAsFirstInstance(Unique))
+                {
+                    App application = new App();
+
+                    application.InitializeComponent();
+                    application.Run();
+
+                    // Allow single instance code to perform cleanup operations
+                    SingleInstance<App>.Cleanup();
+                }
+            }
+            catch(Exception ie)
+            {
+                MessageBox.Show(ie.Message);
+            }
+            
+        }
+
+        #region Implementation of ISingleInstanceApp
+
+        public bool SignalExternalCommandLineArgs(IList<string> args)
+        {
+            return true;
+        }
+
+        #endregion
+    }
+}
---
+++ src/BuildScreen/Plugin/Configuration/PluginElement.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Windows;
+using System.Xml.Serialization;
+using System.Reflection;
+
+namespace BuildScreen.Plugin.Configuration
+{
+    public class PluginElement
+    {
+        [XmlAttribute(AttributeName = "name")]
+        public virtual string Name { get; set; }
+
+        [XmlAttribute(AttributeName = "type")]
+        public string TypeStr { get; set; }
+
+        [XmlIgnore]
+        public Type PluginType
+        {
+            get
+            {
+                return GetTypeFromString();
+            }
+        }
+
+        private Type GetTypeFromString()
+        {
+            Assembly assembly = null;
+            Type pluginType = null;
+
+            try
+            {
+                string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), Name);
+
+                assembly = Assembly.LoadFrom(path);
+
+                Type[] typ = assembly.GetTypes();
+
+                pluginType = assembly.GetType(TypeStr);
+            }
+            catch (Exception ie)
+            {
+                MessageBox.Show(ie.Message);
+            }
+
+            if (pluginType == null)
+                throw new PluginTypeInitializationException(
+                    String.Format("'{0}' could not be found. Check that the type has been typed correctly and the type really exists.",
+                        TypeStr));
+
+            if (pluginType.GetInterface(typeof(IBuildScreenPlugin).FullName) == null)
+                throw new PluginNotSupportedException("Only plugins that implement IBuildScreenPlugin can be added under the plugin section.");
+
+            return pluginType;
+        }
+    }
+
+    [Serializable]
+    internal class PluginNotSupportedException : Exception
+    {
+        public PluginNotSupportedException(string message) : base(message)
+        {
+           
+        }
+    }
+
+    [Serializable]
+    internal class PluginTypeInitializationException : Exception
+    {
+        public PluginTypeInitializationException(string message) : base(message)
+        {
+
+        }
+    }
+}
